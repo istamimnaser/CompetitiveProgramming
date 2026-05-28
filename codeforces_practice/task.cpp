03eb8d31a1be53d9936198f0f5071cec81ebe4be
Tamim Mahrush Naser <itstamimnaser@gmail.com>
2026-05-28 21:30:15 +0600

    28/5
    28/5

--- codeforces_practice/task.cpp
+++ codeforces_practice/task.cpp
@@ -1,2 +1,13 @@
-#include,iostream>
+#include<iostream>
 using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n,a;
+        cin>>n>>a;
+        int v[n];
+  int left = 0, right = 0;
+}
