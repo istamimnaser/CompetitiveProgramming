9ab24f3537eb9a97721f910d002c2d50725538ea
Tamim Mahrush Naser <itstamimnaser@gmail.com>
2026-06-02 21:44:33 +0600

    2/6

---
+++ codeforces_practice/koshary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(x%2!=0 && y%2!=0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+    
+}
